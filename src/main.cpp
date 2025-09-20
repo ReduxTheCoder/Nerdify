96d4145da1ce5bd4c4c9b1b8861d8eeebed90b15
ReduxTheCoder <biondigenny31@gmail.com>
2025-09-20 23:03:19 +0200

    finish program

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 # include <string>
 # include <iostream>
+# include <cctype>
+
+bool super = false;
 
 void help() {
 	std::cout << "Usage:\n";
@@ -8,9 +11,40 @@ void help() {
 	std::cout << "\t-s --super: add '!11!!!' at the end of the string\n";
 }
 
+char upper(char lower) {
+	return static_cast<char>(std::toupper(static_cast<unsigned char>(lower)));
+}
+
+void nerdify(std::string &str) {
+	for (int i = 0; i < str.size(); i++) {
+		if (i % 2 == 0) {
+			str[i] = upper(str[i]);
+		}
+	}
+
+	if (super) {
+		str += "!11!!!";
+	}
+}
+
 int main(int argc, char* argv[]) {
-	if (argc < 2 || argv[1] == "-h" || argv[1] == "--help") {
+	if (argc < 2) {
 		help();
+	}
+
+	for (int current_arg = 1; current_arg < argc; current_arg++) {
+		std::string arg = argv[current_arg]; // create a proper std::string
+		if (arg == "-h" || arg == "--help") {
+			help();
+			continue;
+		}
+		if (arg == "-s" || arg == "--super") {
+			super = true;
+			continue;
+		}
+
+		nerdify(arg);             // pass by reference
+		std::cout << arg << "\n"; // print modified string
 	}
 
 	return 0;
